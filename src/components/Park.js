857421692a6fe99115c671cdebe5000edd7f5b8c
Aspen Wilson <aspen.schell@gmail.com>
2023-05-03 07:10:11 -0700

    formatted park cards

--- src/components/Park.js
+++ src/components/Park.js
@@ -7,17 +7,24 @@ function Park({park, onClickPark}) {
         onClickPark(park)
     }
 
-    const extra = `Establisted: {park.est}`
+
+
+
   return (
-    <div>
-        <Card 
-            onClick= {handleClick}
-            header= {park.title}
-            image = {park.imgUrl}
-            meta= {park.state}
-            description= {park.description}
-            extra={extra}
-        />
+    <div className='park-card'>
+        <Card onClick= {handleClick}>
+        <Card.Content>
+            <Image size='medium' src={park.imgUrl} />
+            </Card.Content>
+            <Card.Header style= {{padding:'5px'}} as='h2'>{park.title}</Card.Header>
+            <Card.Meta style= {{padding:'5px'}}>{park.state}</Card.Meta>
+            <Card.Description style= {{padding:'5px'}}>{park.description}</Card.Description>
+            <Card.Content extra>
+            {'Established: ' + park.est} <br />
+            {'Size: ' + park.acres + ' acres'} <br />
+            {'Yearly visitors: ' + park.visitors}
+            </Card.Content>
+        </Card>
 
     </div>
   )
